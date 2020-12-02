63f8660ad555f53741dd717ee42b18b34401e2d2
Sumit Semwal <sumit.semwal@linaro.org>
2020-12-02 20:40:08 +0530

    android: setup.sh: hack for arm64 worker.
    This should try and understand which worker it's running on, and then
    decide the packages to install accordingly.
    
    Signed-off-by: Sumit Semwal <sumit.semwal@linaro.org>

--- automated/android/noninteractive-tradefed/setup.sh
+++ automated/android/noninteractive-tradefed/setup.sh
@@ -13,12 +13,14 @@ if [ -n "${ANDROID_VERSION}" ] && echo "${ANDROID_VERSION}" | grep -q  "aosp-mas
 #   JDK="openjdk-8-jdk-headless"
 fi
 
-PKG_DEPS="coreutils usbutils curl wget zip xz-utils python-lxml python-setuptools python-pexpect aapt lib32z1-dev libc6-dev-i386 lib32gcc1 libc6:i386 libstdc++6:i386 libgcc1:i386 zlib1g:i386 libncurses5:i386 python-dev python-protobuf protobuf-compiler python-virtualenv python-pip python-pexpect psmisc"
+#PKG_DEPS="coreutils usbutils curl wget zip xz-utils python-lxml python-setuptools python-pexpect aapt lib32z1-dev libc6-dev-i386 lib32gcc1 libc6:i386 libstdc++6:i386 libgcc1:i386 zlib1g:i386 libncurses5:i386 python-dev python-protobuf protobuf-compiler python-virtualenv python-pip python-pexpect psmisc"
+
+PKG_DEPS="coreutils usbutils curl wget zip xz-utils python-lxml python-setuptools python-pexpect aapt lib32z1-dev libc6-dev-arm64-cross lib32gcc1 libc6 libstdc++6 libstdc++6-arm64-cross libgcc1 libgcc1-arm64-cross zlib1g libncurses5 python-dev python-protobuf protobuf-compiler python-virtualenv python-pip python-pexpect psmisc"
 
 dist_name
 case "${dist}" in
     ubuntu)
-        dpkg --add-architecture i386
+#        dpkg --add-architecture i386 # Sumit: removed it for use on arm64 worker
         apt-get update -q
         install_deps "${PKG_DEPS} ${JDK}"
         ;;
